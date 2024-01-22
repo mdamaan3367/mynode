2da6746d460d2474dc408f19282e3fcab4df754a
md amaan <mdamaan3367@gmail.com>
2024-01-22 17:39:10 +0530

    check

---
+++ index.js
@@ -0,0 +1,34 @@
+var admin = require("firebase-admin");
+const express=require('express')
+const app=express()
+
+
+var serviceAccount = require("./olxfirebasereact-31aea-firebase-adminsdk-wlo85-73adf20726.json");
+
+app.use(express.json())
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+app.post('/send-noti',(req,res)=>{
+    console.log(req.body)
+    const message={
+        notification:{
+            title:"new ad",
+            body:"new ad posted click to open"
+        },
+        tokens:req.body.tokens
+    }
+    
+    admin.messaging().sendMulticast(message).then(res=>{
+        console.log("send successful")
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('runnign server')
+})
